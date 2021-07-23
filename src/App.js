0ccfd7b6ac9985c1c5606c3b833064f0dd00c6bb
Emmanuel Ugwu <>
2021-07-23 18:34:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,7 +38,8 @@ function App() {
       <div className="btn-container">
         {
           jobs.map((item, index) => {
-            return (<button key={item.id} onClick={() => setValue(index)}
+            return (
+            <button key={item.id} onClick={() => setValue(index)}
             className={`job-btn ${index === value && 'active-btn'}`}>
               {item.company}
             </button>
